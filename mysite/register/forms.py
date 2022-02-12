b244d54185cd120be84eb6436156937488d0cff8
Chee Yang <cylau005@mymail.sim.edu.sg>
2022-02-12 20:18:28 +0800

    genres

--- mysite/register/forms.py
+++ mysite/register/forms.py
@@ -3,9 +3,33 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+GENRES_CHOICES = [
+    ('Adventure','Adventure'),
+    ('Comedy','Comedy'),
+    ('Action','Action'),
+    ('Drama','Drama'),
+    ('Crime','Crime'),
+    ('Children','Children'),
+    ('Mystery','Mystery'),
+    ('Documentary','Documentary'),
+    ('Animation','Animation'),
+    ('Thriller','Thriller'),
+    ('Horror','Horror'),
+    ('Fantasy','Fantasy'),
+    ('Western','Western'),
+    ('Film-Noir','Film-Noir'),
+    ('Romance','Romance'),
+    ('War','War'),
+    ('Sci-Fi','Sci-Fi'),
+    ('Musical','Musical'),
+]
+
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
+    genres = forms.CharField(label='What is your favorite genres?', 
+            widget=forms.Select(choices=GENRES_CHOICES))
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "password1", "password2","first_name","last_name","genres"]
