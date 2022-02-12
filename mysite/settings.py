17f75bfa5c7dbeffed916273c6419af7e75ccd37
cylau005 <99126850+cylau005@users.noreply.github.com>
2022-02-12 14:18:10 +0800

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,15 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static')
-]
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
