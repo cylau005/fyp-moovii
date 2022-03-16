68b233cd0c6715c0cdfb0b1739fc244d28de1cae
Chee Yang <cylau005@mymail.sim.edu.sg>
2022-03-16 21:51:25 +0800

    frontend skeleton

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -19,7 +19,7 @@ auth_views.PasswordResetView.as_view(template_name="main/password_reset.html"), 
 path('reset_password_sent/', 
 auth_views.PasswordResetDoneView.as_view(template_name="main/password_reset_sent.html"), name="password_reset_done"),
 path('reset/<uidb64>/<token>/', 
-auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_form.html"), name="password_reset_confirm",),
+auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_form.html"), name="password_reset_confirm"),
 path('reset_password_complete/', 
 auth_views.PasswordResetCompleteView.as_view(template_name="main/password_reset_done.html"), name="password_reset_complete"),
 ]
