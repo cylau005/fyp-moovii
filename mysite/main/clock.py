ab68fc64c2a7e172c70b85648e67909da08e9583
Chee Yang <cylau005@mymail.sim.edu.sg>
2022-03-31 21:10:09 +0800

    schedule

--- mysite/main/clock.py
+++ mysite/main/clock.py
@@ -5,7 +5,7 @@ import schedule
 from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', days=1)
 def cr():
     print('do crawler') 
     os.system("python main/imdbCrawl.py") 
